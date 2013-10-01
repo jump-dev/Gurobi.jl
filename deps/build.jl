8be7f6ef736fc5ba9c71ce2613a1cc160e7acfcf
Miles Lubin <miles.lubin@gmail.com>
2013-10-01 14:08:58 -0400

    fixes for windows; support gurobi 5.6

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,7 @@ using BinDeps
 
 @BinDeps.setup
 
-libgurobi = library_dependency("libgurobi",aliases=["libgurobi51","libgurobi55"])
+libgurobi = library_dependency("libgurobi",aliases=["libgurobi51","gurobi51","libgurobi55","gurobi55","libgurobi56","gurobi56"])
 
-provides(Binaries, joinpath(ENV["GUROBI_HOME"],"lib"), libgurobi)
+@unix_only provides(Binaries, joinpath(ENV["GUROBI_HOME"],"lib"), libgurobi)
+@windows_only provides(Binaries, joinpath(ENV["GUROBI_HOME"],"bin"), libgurobi)
