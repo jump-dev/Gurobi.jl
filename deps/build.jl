a9e74f56295dbb3c2669b76ee6ed87f530e3b908
Miles Lubin <miles.lubin@gmail.com>
2013-10-02 09:54:40 -0400

    fix library finding on OS X. Closes #7

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using BinDeps
 
 @BinDeps.setup
 
-libgurobi = library_dependency("libgurobi",aliases=["libgurobi51","gurobi51","libgurobi55","gurobi55","libgurobi56","gurobi56"])
+libgurobi = library_dependency("libgurobi",aliases=["libgurobi51.so","gurobi51","libgurobi55.so","gurobi55","libgurobi56.so","gurobi56"])
 
 @unix_only provides(Binaries, joinpath(ENV["GUROBI_HOME"],"lib"), libgurobi)
 @windows_only provides(Binaries, joinpath(ENV["GUROBI_HOME"],"bin"), libgurobi)
