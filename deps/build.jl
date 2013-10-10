f6644c761b535cd9223b0552ba8614d8bbcad57c
Miles Lubin <miles.lubin@gmail.com>
2013-10-10 12:57:16 -0400

    if GUROBI_HOME isn't set, just look in standard paths on unix

--- deps/build.jl
+++ deps/build.jl
@@ -4,5 +4,7 @@ using BinDeps
 
 libgurobi = library_dependency("libgurobi",aliases=["libgurobi51.so","gurobi51","libgurobi55.so","gurobi55","libgurobi56.so","gurobi56"])
 
-@unix_only provides(Binaries, joinpath(ENV["GUROBI_HOME"],"lib"), libgurobi)
+if haskey(ENV, "GUROBI_HOME")
+    @unix_only provides(Binaries, joinpath(ENV["GUROBI_HOME"],"lib"), libgurobi)
+end
 @windows_only provides(Binaries, joinpath(ENV["GUROBI_HOME"],"bin"), libgurobi)
