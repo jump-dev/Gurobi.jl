1059a6ea8a46eb5f9886f4911499bf6feeff2b79
Dahua Lin <lindahua@gmail.com>
2013-10-16 11:34:54 -0500

    add Gurobi.version

--- src/Gurobi.jl
+++ src/Gurobi.jl
@@ -3,8 +3,8 @@ module Gurobi
 using BinDeps
 @BinDeps.load_dependencies
 
-	# Standard LP interface
-	require(joinpath(Pkg.dir("MathProgBase"),"src","LinprogSolverInterface.jl"))
+    # Standard LP interface
+    require(joinpath(Pkg.dir("MathProgBase"),"src","LinprogSolverInterface.jl"))
     importall LinprogSolverInterface
 
     export set_int_param!, set_dbl_param!, get_int_param, get_dbl_param
@@ -15,7 +15,7 @@ using BinDeps
     export add_var!, add_vars!, add_cvar!, add_cvars!
     export add_bvar!, add_bvars!, add_ivar!, add_ivars!
     export add_constr!, add_constrs!, add_qpterms!, add_qconstr!
-	export add_rangeconstr!, add_rangeconstrs!
+    export add_rangeconstr!, add_rangeconstrs!
     
     export qp_model
     export optimize, computeIIS
@@ -25,6 +25,7 @@ using BinDeps
     
     import Base.convert, Base.show, Base.copy
     
+    # macro to call a Gurobi C function
     macro grb_ccall(func, args...)
         f = "GRB$(func)"
         quote
@@ -32,10 +33,25 @@ using BinDeps
         end
     end
 
+    # Gurobi library version
+    function getlibversion()
+        _major = Cint[0]
+        _minor = Cint[0]
+        _tech = Cint[0]
+        @grb_ccall(version, Void, (Ptr{Cint}, Ptr{Cint}, Ptr{Cint}), _major, _minor, _tech)
+        return VersionNumber(_major[1], _minor[1], _tech[1])        
+    end
+
+    # version need not be export
+    # one can write Gurobi.version to get the version numbers
+    const version = getlibversion()
+
+    # include source files
+
     include("grb_env.jl")
     include("grb_model.jl")
     include("grb_solve.jl")
     include("grb_callbacks.jl")
 
-	include("GurobiSolverInterface.jl")
+    include("GurobiSolverInterface.jl")
 end
