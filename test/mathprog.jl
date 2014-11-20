3e149ae88404d16589b8e26d1170cbd5ac3ffde7
Miles Lubin <miles.lubin@gmail.com>
2014-11-19 20:45:30 -0500

    implement QCP dual interface

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -13,6 +13,7 @@ mixintprogtest(GurobiSolver())
 include(joinpath(Pkg.dir("MathProgBase"),"test","quadprog.jl"))
 quadprogtest(GurobiSolver())
 socptest(GurobiSolver())
+qpdualtest(GurobiSolver(QCPDual=1))
 
 include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
 coniclineartest(GurobiSolver())
