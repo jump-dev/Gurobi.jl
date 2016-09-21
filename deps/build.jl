463cae901800f93416401bb9a62ec4c39d623e97
Robert Schwarz <mail@rschwarz.net>
2016-09-21 18:17:39 +0200

    fix deprecation warnings in build.jl (for 0.5.0)

--- deps/build.jl
+++ deps/build.jl
@@ -15,11 +15,17 @@ paths_to_try = copy(aliases)
 
 for a in aliases
     if haskey(ENV, "GUROBI_HOME")
-        @unix_only push!(paths_to_try, joinpath(ENV["GUROBI_HOME"],"lib",string("lib",a,".so")))
-        @windows_only push!(paths_to_try, joinpath(ENV["GUROBI_HOME"],"bin",string(a,".",Libdl.dlext)))
+        @static if is_unix()
+            push!(paths_to_try, joinpath(ENV["GUROBI_HOME"],"lib",string("lib",a,".so")))
+        end
+        @static if is_windows()
+            push!(paths_to_try, joinpath(ENV["GUROBI_HOME"],"bin",string(a,".",Libdl.dlext)))
+        end
     end
     # gurobi uses .so on OS X for some reason
-    @osx_only push!(paths_to_try, string("lib$a.so"))
+    @static if is_apple()
+        push!(paths_to_try, string("lib$a.so"))
+    end
 end
 
 found = false
