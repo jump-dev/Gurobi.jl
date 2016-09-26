4ee698b8cdbf71451779ab24c6134d8cd616a43b
odow <odow003@aucklanduni.ac.nz>
2016-09-27 08:09:44 +1300

    Add support for Gurobi 7.0

--- deps/build.jl
+++ deps/build.jl
@@ -11,7 +11,7 @@ function write_depsfile(path)
     close(f)
 end
 
-aliases = ["gurobi65","gurobi60","gurobi56","gurobi55"]
+aliases = ["gurobi70","gurobi65","gurobi60","gurobi56","gurobi55"]
 
 paths_to_try = copy(aliases)
 
