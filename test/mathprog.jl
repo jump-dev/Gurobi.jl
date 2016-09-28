eb15908c640875c33fbb30186c9ba96fd3dc75d0
Miles Lubin <miles.lubin@gmail.com>
2016-09-28 16:13:02 -0400

    implement solver-independent parameters (#65)

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -3,8 +3,11 @@ using Gurobi
 include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
 linprogtest(GurobiSolver(InfUnbdInfo=1))
 
+s = GurobiSolver()
+MathProgBase.setparameters!(s, Silent=true, TimeLimit=100.0)
+
 include(joinpath(Pkg.dir("MathProgBase"),"test","linproginterface.jl"))
-linprogsolvertest(GurobiSolver())
+linprogsolvertest(s)
 
 include(joinpath(Pkg.dir("MathProgBase"),"test","mixintprog.jl"))
 mixintprogtest(GurobiSolver())
