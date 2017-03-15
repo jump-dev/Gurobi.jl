5555f747a7a3283779495c72674b696ec492cce7
iaravena <ignacio.aravena@uclouvain.be>
2017-03-15 19:41:24 +0100

    Update test_get_strarray.jl

--- test/test_get_strarray.jl
+++ test/test_get_strarray.jl
@@ -9,13 +9,5 @@ m = Gurobi.Model(env, "test")
 read_model(m, "test_get_strarray.mps")
 
 # test functions
-@test Gurobi.get_strattrarray(m, "VarName", 1, Gurobi.num_vars(m))[1] == "XONE"
-@test Gurobi.get_strattrarray(m, "ConstrName", 1, Gurobi.num_constrs(m))[1] =="LIM1"
-
-# get variable and constraint names
-varnames = Gurobi.get_strattrarray(m, "VarName", 1, Gurobi.num_vars(m))
-connames = Gurobi.get_strattrarray(m, "ConstrName", 1, Gurobi.num_constrs(m))
-
-# print variable and constraint names
-println(varnames)		# should output ["XONE","YTWO","ZTHREE"]
-println(connames)		# should output ["LIM1","LIM2","MYEQN"]
+@test Gurobi.get_strattrarray(m, "VarName", 1, Gurobi.num_vars(m)) == ["XONE","YTWO","ZTHREE"]
+@test Gurobi.get_strattrarray(m, "ConstrName", 1, Gurobi.num_constrs(m)) == ["LIM1","LIM2","MYEQN"]
