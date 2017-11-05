1283a3be85feae904b3a4bbc673d7cffe7a0279c
Robin Deits <robin.deits@gmail.com>
2017-11-05 17:32:34 -0500

    make it possible to silence gurobi environment construction

--- src/grb_env.jl
+++ src/grb_env.jl
@@ -3,11 +3,40 @@
 
 type Env
     ptr_env::Ptr{Void}
-    
-    function Env()
+
+    """
+        Env(silent::Bool=false)
+
+    Construct a Gurobi environment. The environment maintains parameters of the
+    Gurobi solver and its license. In particular, constructing an environment
+    will obtain a new Gurobi license token. If you are operating in a setting
+    where the number of concurrent Gurobi tokens is limited, you may want to
+    construct a single environment ahead of time and share it among all the
+    models solved by your program.
+
+    The `silent` option temporarily redirects the standard output stream
+    while constructing the environment. This may be useful in preventing
+    Gurobi from printing messages to the screen every time an environment
+    is constructed.
+    """
+    function Env(silent::Bool=false)
         a = Array{Ptr{Void}}(1)
-        ret = @grb_ccall(loadenv, Cint, (Ptr{Ptr{Void}}, Ptr{UInt8}), 
-            a, C_NULL)
+        if silent
+            _stdout = STDOUT
+            stdout_rd, stdout_wr = redirect_stdout()
+            ret = try
+                @grb_ccall(loadenv, Cint, (Ptr{Ptr{Void}}, Ptr{UInt8}),
+                    a, C_NULL)
+            finally
+                redirect_stdout(_stdout)
+                close(stdout_rd)
+                close(stdout_wr)
+            end
+        else
+            ret = @grb_ccall(loadenv, Cint, (Ptr{Ptr{Void}}, Ptr{UInt8}),
+                a, C_NULL)
+        end
+
         if ret != 0
             if ret == 10009
                 error("Invalid Gurobi license")
@@ -45,7 +74,7 @@ end
 type GurobiError
     code::Int
     msg::String
-    
+
     function GurobiError(env::Env, code::Integer)
         new(convert(Int, code), get_error_msg(env))
     end
