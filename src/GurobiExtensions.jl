be5acb25d03e417d3e4ad0e0e17a0040ca1c7e88
odow <o.dowson@gmail.com>
2019-02-10 19:43:11 -0600

    Unify structure with GLPK

--- src/GurobiExtensions.jl
+++ src/GurobiExtensions.jl
@@ -3,17 +3,24 @@ module GurobiExtensions
 using Gurobi, JuMP
 
 export @lazy_constraint
+export set_callbacks, add_lazy_constraint, add_heuristic_solution
 
+"""
+    @lazy_constraint(model, cb_data, expr)
+
+Add a lazy constraint (where the constraint is given by `expr`) to `model`.
+
+This can be called *only* from a lazy callback set by `JuMP.set_callbacks`.
+
+### Examples
+
+    @lazy_constraint(model, cb_data, 2x + y <+ 1)
+"""
 macro lazy_constraint(model, cb_data, expr)
     code = quote
         lazy_con = @build_constraint $expr
-        JuMP.backend($model)
-        Gurobi.cblazy(
-            JuMP.backend($model),
-            $cb_data,
-            JuMP.moi_function(lazy_con.func),
-            lazy_con.set
-        )
+        add_lazy_constraint($model, $cb_data, JuMP.moi_function(lazy_con.func),
+                            lazy_con.set)
     end
     quote
         let
@@ -22,21 +29,79 @@ macro lazy_constraint(model, cb_data, expr)
     end
 end
 
-function Gurobi.cbget_mipsol_sol(
-        model::JuMP.Model, cb_data::Gurobi.CallbackData, cb_where::Int32)
-    Gurobi.cbget_mipsol_sol(JuMP.backend(model), cb_data, cb_where)
+"""
+    set_callbacks(model::Model; lazy = nothing, heuristic = nothing)
+
+Set an (optional) lazy and heuristic callback. This can be used only when
+`JuMP.mode(mode) == DIRECT`.
+
+Each callback must be a function that takes two arguments: the backend
+`optimizer` object (returned by `JuMP.backend(model)`), and a solver-specific
+type `cb_data`.
+
+### Example
+
+    set_callbacks(model;
+        lazy = (optimizer, cb_data) -> println("Called from lazy callback.")
+    )
+"""
+function set_callbacks(model::Model; lazy = nothing, heuristic = nothing)
+    if JuMP.mode(model) != JuMP.DIRECT
+        error("You must use a solver in DIRECT mode to use callbacks in JuMP.")
+    end
+    _set_callbacks(model, JuMP.backend(model), lazy, heuristic)
 end
 
-function Gurobi.cbget_mipnode_rel(
-        model::JuMP.Model, cb_data::Gurobi.CallbackData, cb_where::Int32)
-    Gurobi.cbget_mipnode_rel(JuMP.backend(model), cb_data, cb_where)
+function _set_callbacks(model, optimizer, lazy, heuristic)
+    error("The model $(typeof(optimizer)) does not support callbacks.")
 end
 
-function Gurobi.cbsolution(model::JuMP.Model, cb_data::Gurobi.CallbackData,
-                           sol::Dict{JuMP.VariableRef, Float64})
-    moi_sol = Dict{MOI.VariableIndex, Float64}(
-        JuMP.index(variable) => value for (variable, value) in sol)
-    Gurobi.cbsolution(JuMP.backend(model), cb_data, moi_sol)
+"""
+    add_lazy_constraint(model, cb_data, func, set)
+
+Add a lazy constraint `func`-in-`set` to `model`.
+
+This can be called only from a lazy callback set by `set_callbacks`.
+"""
+function add_lazy_constraint end
+
+
+"""
+    add_heuristic_solution(model, cb_data, sol::Dict{JuMP.VariableRef, Float64})
+
+Provide the heuristic solution given by the variable-value mapping of `sol` to
+`model`.
+
+This can be called only from a heuristic callback set by `set_callbacks`.
+"""
+function add_heuristic_solution end
+
+###
+### Gurobi specific functionality.
+###
+
+function _set_callbacks(model::Model, optimizer::Gurobi.Optimizer,
+                        lazy_callback, heuristic_callback)
+    MOI.set(optimizer, Gurobi.CallbackFunction(), (cb_data, cb_where) -> begin
+        if lazy_callback !== nothing && cb_where == Gurobi.CB_MIPSOL
+            Gurobi.cbget_mipsol_sol(optimizer, cb_data, cb_where)
+            lazy_callback(model, cb_data)
+        elseif heuristic_callback !== nothing && cb_where == Gurobi.CB_MIPNODE
+            Gurobi.cbget_mipnode_rel(optimizer, cb_data, cb_where)
+            heuristic_callback(model, cb_data)
+        end
+        return
+    end)
+end
+
+function add_lazy_constraint(model::Model, cb_data::Gurobi.CallbackData, func, set)
+    Gurobi.cblazy(JuMP.backend(model), cb_data, func, set)
+end
+
+function add_heuristic_solution(model::Model, cb_data::Gurobi.CallbackData,
+                                sol::Dict{JuMP.VariableRef, Float64})
+    Gurobi.cbsolution(JuMP.backend(model), cb_data, Dict(
+        JuMP.index(variable) => value for (variable, value) in sol))
 end
 
 end
--- test/callbacks.jl
+++ test/callbacks.jl
@@ -24,62 +24,49 @@ model = JuMP.direct_model(
 
 # This vector is going to cache the value of `cb_where` everytime our callback
 # gets called.
-cb_calls = Int32[]
+cb_calls = Symbol[]
 
-# Here is the callback function. We set the solver-specific attribute
-# `Gurobi.CallbackFunction()`, passing a function (in this case, anonymous) as
-# the third argument.
-MOI.set(model, Gurobi.CallbackFunction(), (cb_data, cb_where) -> begin
-    # Cache the value of `cb_where` in `cb_calls`. Note how we can access
-    # variables in the outer scope.
-    push!(cb_calls, cb_where)
-    if cb_where == Gurobi.CB_MIPSOL
-        # Gurobi has a feasible integer solution to the current problem.
-        # Load the intger solution into the model. This lets us query
-        # `JuMP.value` on variables.
-        Gurobi.cbget_mipsol_sol(model, cb_data, cb_where)
-        # Double check for sanity's sake that we have a feasible (given the
-        # current constraints) point.
-        @assert JuMP.primal_status(model) == MOI.FEASIBLE_POINT
-        # Get the values of x and y.
-        x_val, y_val = JuMP.value(x), JuMP.value(y)
-        # Add the lazy constraints using the `@lazy_constraint` macro that was
-        # loaded by `Gurobi.@load_extensions`.
-        if y_val - x_val > 1 + 1e-6
-            @lazy_constraint(model, cb_data, y <= 1 + x)
-        elseif y_val + x_val > 3 + 1e-6
-            @lazy_constraint(model, cb_data, y <= 3 - x)
-        end
-    elseif cb_where == Gurobi.CB_MIPNODE
-        # Gurobi has a fractional solution to the current problem.
-        # Load the solution into the model.
-        Gurobi.cbget_mipnode_rel(model, cb_data, cb_where)
-        # Double check for sanity's sake that we have a feasible (given the
-        # current constraints) point.
-        @assert JuMP.primal_status(model) == MOI.FEASIBLE_POINT
-        # Get the values of x and y.
-        x_val, y_val = JuMP.value(x), JuMP.value(y)
-        # Provide a heuristic solution. We don't need to provide a value for all
-        # variables.
-        Gurobi.cbsolution(model, cb_data, Dict(x => round(x_val)))
+function my_lazy_callback(model, cb_data)
+    push!(cb_calls, :lazy)
+    # Double check for sanity's sake that we have a feasible (given the
+    # current constraints) point.
+    @assert primal_status(model) == MOI.FEASIBLE_POINT
+    # Get the values of x and y.
+    x_val, y_val = value(x), value(y)
+    # Add the lazy constraints using the `@lazy_constraint` macro that was
+    # loaded by `@load_extensions`.
+    if y_val - x_val > 1.1 + 1e-6
+        @lazy_constraint(model, cb_data, y <= 1.1 + x)
+    elseif y_val + x_val > 3 + 1e-6
+        @lazy_constraint(model, cb_data, y <= 3 - x)
     end
-    return
-end)
+end
+
+function my_heuristic_callback(model, cb_data)
+    push!(cb_calls, :heuristic)
+    # Double check for sanity's sake that we have a feasible (given the
+    # current constraints) point.
+    @assert primal_status(model) == MOI.FEASIBLE_POINT
+    # Get the values of x and y.
+    x_val, y_val = value(x), value(y)
+    # Provide a heuristic solution.
+    add_heuristic_solution(
+        model, cb_data, Dict(x => 1.0, y => 2.0))
+end
+
+set_callbacks(model;
+    lazy = my_lazy_callback,
+    heuristic = my_heuristic_callback
+)
 
 # Solve the model.
-JuMP.optimize!(model)
+optimize!(model)
 
 # Check the solution.
-@test JuMP.value(x) == 1
-@test JuMP.value(y) == 2
+@test value(x) == 1
+@test value(y) == 2
 
 # Check that our callback has been used.
 @test length(cb_calls) > 0
-@test Gurobi.CB_POLLING in cb_calls    # A frequent callback to return control to the user
-@test Gurobi.CB_PRESOLVE in cb_calls   # Entered presolve
-@test !(Gurobi.CB_SIMPLEX in cb_calls) # Using the simplex method
-@test Gurobi.CB_MIP in cb_calls        # Entered a MIP
-@test Gurobi.CB_MIPSOL in cb_calls     # Found an integer solution
-@test Gurobi.CB_MIPNODE in cb_calls    # Found a fractional solution
-@test Gurobi.CB_MESSAGE in cb_calls    # Printing a message to the log
-@test !(Gurobi.CB_BARRIER in cb_calls) # Using the barrier method
+@test :lazy in cb_calls
+@test :heuristic in cb_calls
