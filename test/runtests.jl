b3ea69617c45f2bc0e4cf8d805f4677ceabb04f3
odow <o.dowson@gmail.com>
2019-06-09 19:50:53 -0500

    Re-add commented tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,17 @@
 using Gurobi, Test, Random
 
-# @testset "C API" begin
-#     include("c_wrapper.jl")
-# end
+@testset "C API" begin
+    include("c_wrapper.jl")
+end
 
-# @testset "MathProgBase Tests" begin
-#     @testset for file in ["env", "large_coefficients", "range_constraints",]
-#         evalfile(joinpath("MathProgBase", "$(file).jl"))
-#     end
-#     @testset "MathProgJuMP" begin
-#         evalfile(joinpath("MathProgBase", "mathprog.jl"))
-#     end
-# end
+@testset "MathProgBase Tests" begin
+    @testset for file in ["env", "large_coefficients", "range_constraints",]
+        evalfile(joinpath("MathProgBase", "$(file).jl"))
+    end
+    @testset "MathProgJuMP" begin
+        evalfile(joinpath("MathProgBase", "mathprog.jl"))
+    end
+end
 
 @testset "MathOptInterface Tests" begin
     include("MOI_wrapper.jl")
