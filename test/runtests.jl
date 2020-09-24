e1b25909548b3902014125367bba2191d7dc0271
odow <o.dowson@gmail.com>
2020-09-25 11:50:55 +1200

    Add deprecation tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,9 @@ using Test
         include(joinpath("MOI", file))
     end
 end
+
+@testset "Deprecated functions" begin
+    err = ErrorException(Gurobi._DEPRECATED_ERROR_MESSAGE)
+    @test_throws err get_status()
+    @test_throws err Gurobi.get_status_code()
+end
