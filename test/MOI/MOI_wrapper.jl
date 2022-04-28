466915962d32c228af89a8125d1037859083c9ab
rafabench <rafa.bench@gmail.com>
2022-04-28 15:31:11 -0300

    add tests

--- test/MOI/MOI_wrapper.jl
+++ test/MOI/MOI_wrapper.jl
@@ -647,6 +647,61 @@ function test_log_file()
     return
 end
 
+function test_multiple_modifications()
+    model = Gurobi.Optimizer(GRB_ENV)
+
+    x = MOI.add_variables(model, 3)
+
+    saf = MOI.ScalarAffineFunction(
+        [
+            MOI.ScalarAffineTerm(1.0, x[1]),
+            MOI.ScalarAffineTerm(1.0, x[2]),
+            MOI.ScalarAffineTerm(1.0, x[3]),
+        ],
+        0.0,
+    )
+    ci1 = MOI.add_constraint(model, saf, MOI.LessThan(1.0))
+    ci2 = MOI.add_constraint(model, saf, MOI.LessThan(2.0))
+
+    MOI.set(model, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), saf)
+
+    fc1 = MOI.get(model, MOI.ConstraintFunction(), ci1)
+    @test MOI.coefficient.(fc1.terms) == [1.0, 1.0, 1.0]
+    fc2 = MOI.get(model, MOI.ConstraintFunction(), ci2)
+    @test MOI.coefficient.(fc2.terms) == [1.0, 1.0, 1.0]
+    obj = MOI.get(
+        model,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+    )
+    @test MOI.coefficient.(obj.terms) == [1.0, 1.0, 1.0]
+
+    changes_cis = [
+        MOI.ScalarCoefficientChange(MOI.VariableIndex(1), 4.0)
+        MOI.ScalarCoefficientChange(MOI.VariableIndex(1), 0.5)
+        MOI.ScalarCoefficientChange(MOI.VariableIndex(3), 2.0)
+    ]
+    MOI.modify(model, [ci1, ci2, ci2], changes_cis)
+
+    fc1 = MOI.get(model, MOI.ConstraintFunction(), ci1)
+    @test MOI.coefficient.(fc1.terms) == [4.0, 1.0, 1.0]
+    fc2 = MOI.get(model, MOI.ConstraintFunction(), ci2)
+    @test MOI.coefficient.(fc2.terms) == [0.5, 1.0, 2.0]
+
+    changes_obj = [
+        MOI.ScalarCoefficientChange(MOI.VariableIndex(1), 4.0)
+        MOI.ScalarCoefficientChange(MOI.VariableIndex(2), 10.0)
+        MOI.ScalarCoefficientChange(MOI.VariableIndex(3), 2.0)
+    ]
+    MOI.modify(
+        model,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        changes_obj,
+    )
+
+    obj = MOI.get(model, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}())
+    @test MOI.coefficient.(obj.terms) == [4.0, 10.0, 2.0]
+end
+
 end
 
 TestMOIWrapper.runtests()
