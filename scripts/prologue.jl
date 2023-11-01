399ee7ecb2e92776b7a706f6e8f352d60bb450b8
Oscar Dowson <odow@users.noreply.github.com>
2023-11-02 08:21:35 +1300

    Update prologue.jl

--- scripts/prologue.jl
+++ scripts/prologue.jl
@@ -6,18 +6,17 @@
 
 # !format: off
 
+# ============================ start of prologue.jl ============================
+# These constants are handled explicitly in the prologue.jl to avoid them being
+# automatically parsed as `const GRB_LESS_EQUAL = Cchar('<')`. There's probably
+# a better way to handle this, but it works for now, and they won't be changing
+# in future releases.
 const GRB_LESS_EQUAL = '<'
-
 const GRB_GREATER_EQUAL = '>'
-
 const GRB_EQUAL = '='
-
 const GRB_CONTINUOUS = 'C'
-
 const GRB_BINARY = 'B'
-
 const GRB_INTEGER = 'I'
-
 const GRB_SEMICONT = 'S'
-
 const GRB_SEMIINT = 'N'
+# ============================= end of prologue.jl =============================
