db914567e2d574a4a558bf8f3724a716a9b7d29c
Simon Bowly <bowly@gurobi.com>
2023-11-02 18:19:23 +1100

    Support for Gurobi v11 (#531)

--- src/Gurobi.jl
+++ src/Gurobi.jl
@@ -60,10 +60,13 @@ elseif _is_patch(_GUROBI_VERSION, v"9.5")
     include("gen95/libgrb_api.jl")
 elseif _is_patch(_GUROBI_VERSION, v"10.0")
     include("gen100/libgrb_api.jl")
+elseif _is_patch(_GUROBI_VERSION, v"11.0")
+    include("gen110/libgrb_api.jl")
 else
     error("""
     You have installed version $_GUROBI_VERSION of Gurobi, which is not
-    supported by Gurobi.jl. We require Gurobi version 9.0 or 9.1 or 9.5 or 10.0.
+    supported by Gurobi.jl. We require Gurobi version 9.0 or 9.1 or 9.5
+    or 10.0 or 11.0.
 
     After installing a supported version of Gurobi, run:
 
