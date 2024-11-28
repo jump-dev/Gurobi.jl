4ac5c82734df9fde44badffede5f52cc334467c9
Oscar Dowson <odow@users.noreply.github.com>
2024-11-29 10:59:23 +1300

    Update MOI_wrapper.jl

--- test/MOI/MOI_wrapper.jl
+++ test/MOI/MOI_wrapper.jl
@@ -1460,14 +1460,21 @@ function test_scalar_quadratic_function_with_off_diag_in_scalar_nonlinear()
     if !Gurobi._supports_nonlinear()
         return
     end
-    model = Gurobi.Optimizer(GRB_ENV)
-    MOI.set(model, MOI.Silent(), true)
-    x, _ = MOI.add_constrained_variable(model, MOI.EqualTo(2.0))
-    y, _ = MOI.add_constrained_variable(model, MOI.EqualTo(3.0))
-    f = MOI.ScalarNonlinearFunction(:sqrt, Any[1.0*x*y])
-    MOI.add_constraint(model, f, MOI.GreaterThan(2.0))
-    MOI.optimize!(model)
-    @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
+    for (a, b, status) in [
+        (1.0, 2.0, MOI.OPTIMAL),
+        (1.0, 3.0, MOI.INFEASIBLE),
+        (2.0, 3.0, MOI.OPTIMAL),
+        (2.0, 4.0, MOI.INFEASIBLE),
+    ]
+        model = Gurobi.Optimizer(GRB_ENV)
+        MOI.set(model, MOI.Silent(), true)
+        x, _ = MOI.add_constrained_variable(model, MOI.EqualTo(2.0))
+        y, _ = MOI.add_constrained_variable(model, MOI.EqualTo(3.0))
+        f = MOI.ScalarNonlinearFunction(:sqrt, Any[a*x*y])
+        MOI.add_constraint(model, f, MOI.GreaterThan(b))
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == status
+    end
     return
 end
 
