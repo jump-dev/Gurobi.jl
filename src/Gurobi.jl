8acfa74e55d9f009994c352f0994e4b2b9865431
odow <o.dowson@gmail.com>
2025-02-21 09:13:01 +1300

    Add PrecompileTools.jl

--- src/Gurobi.jl
+++ src/Gurobi.jl
@@ -113,4 +113,56 @@ for sym in filter(s -> startswith("$s", "GRB"), names(@__MODULE__, all = true))
     @eval export $sym
 end
 
+import PrecompileTools
+
+PrecompileTools.@setup_workload begin
+    PrecompileTools.@compile_workload begin
+        let
+            model = MOI.Utilities.CachingOptimizer(
+                MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+                MOI.instantiate(Gurobi.Optimizer; with_bridge_type = Float64),
+            )
+            MOI.set(model, MOI.Silent(), true)
+            x = MOI.add_variables(model, 3)
+            MOI.supports(model, MOI.VariableName(), typeof(x[1]))
+            MOI.set(model, MOI.VariableName(), x[1], "x1")
+            MOI.set(model, MOI.VariablePrimalStart(), x[1], 0.0)
+            MOI.add_constraint(model, x[1], MOI.ZeroOne())
+            MOI.add_constraint(model, x[2], MOI.Integer())
+            for F in (MOI.VariableIndex, MOI.ScalarAffineFunction{Float64})
+                MOI.supports_constraint(model, F, MOI.GreaterThan{Float64})
+                MOI.supports_constraint(model, F, MOI.LessThan{Float64})
+                MOI.supports_constraint(model, F, MOI.EqualTo{Float64})
+            end
+            MOI.supports_constraint(model, MOI.VariableIndex, MOI.ZeroOne)
+            MOI.supports_constraint(model, MOI.VariableIndex, MOI.Integer)
+            MOI.add_constraint(model, x[1], MOI.GreaterThan(0.0))
+            MOI.add_constraint(model, x[2], MOI.LessThan(0.0))
+            MOI.add_constraint(model, x[3], MOI.EqualTo(0.0))
+            MOI.add_constrained_variable(model, MOI.GreaterThan(0.0))
+            MOI.add_constrained_variable(model, MOI.LessThan(0.0))
+            MOI.add_constrained_variable(model, MOI.EqualTo(0.0))
+            MOI.add_constrained_variable(model, MOI.Integer())
+            MOI.add_constrained_variable(model, MOI.ZeroOne())
+            set = (MOI.GreaterThan(0.0), MOI.LessThan(0.0))
+            MOI.supports_add_constrained_variable(model, typeof(set))
+            MOI.add_constrained_variable(model, set)
+            f = 1.0 * x[1] + x[2] + x[3]
+            c1 = MOI.add_constraint(model, f, MOI.GreaterThan(0.0))
+            MOI.set(model, MOI.ConstraintName(), c1, "c1")
+            MOI.supports(model, MOI.ConstraintName(), typeof(c1))
+            MOI.add_constraint(model, f, MOI.LessThan(0.0))
+            MOI.add_constraint(model, f, MOI.EqualTo(0.0))
+            MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+            MOI.supports(model, MOI.ObjectiveFunction{typeof(f)}())
+            MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+            MOI.optimize!(model)
+            MOI.get(model, MOI.TerminationStatus())
+            MOI.get(model, MOI.PrimalStatus())
+            MOI.get(model, MOI.DualStatus())
+            MOI.get(model, MOI.VariablePrimal(), x)
+        end
+    end
+end
+
 end
